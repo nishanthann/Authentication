3ee4efe37e19ef6ed7d76d40d8f8a7db198add47
nishanthann <nizhanth23@gmail@gmail.com>
2025-08-07 13:04:48 +0530

    and verifyToken middleware for user feat(auth): add checkAuth endpoint authentication

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import { connectDB } from "./db/connectDB.js";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/auth", authRoutes);
 
 app.listen(3000, () => {
