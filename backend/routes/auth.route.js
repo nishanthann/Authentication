c5ce1dac14994b9b6d47e5d1a432b5ae3012aba9
nishanthann <nizhanth23@gmail@gmail.com>
2025-08-07 12:39:48 +0530

    feat(auth): enhance email functionality with verification, welcome, and password reset emails

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { Login, logout, Signup } from "../controllers/auth.controller.js";
+import {
+  Login,
+  Logout,
+  Signup,
+  verifyEmail,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", Signup);
 router.post("/login", Login);
-router.post("/logout", logout);
-
+router.post("/logout", Logout);
+router.post("/verify-email", verifyEmail);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 export default router;
